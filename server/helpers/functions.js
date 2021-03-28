9437e6c19923b4fa15cba911793ef46902cad7fc
git <gitit1@msn.com>
2021-03-28 11:01:07 +0300

    git commit -m "fixed missing getUserData func"

--- server/helpers/functions.js
+++ server/helpers/functions.js
@@ -1,4 +1,7 @@
 
+const chalk = require('chalk');
+const log = console.log;
+const User = require('../models/User');
 
 exports.getTodayTime = () => {
     var date = new Date();
@@ -7,3 +10,13 @@ exports.getTodayTime = () => {
     return time;
 }
 
+exports.getUserData = (userEmail) => {
+    log(chalk.bgYellow.black('[Controller] getUserData'));
+    return new Promise(function (resolve, reject) {
+        User.findOne({ email: userEmail }, function (err, user) {
+            if (err) reject(err)
+            if (!user) resolve(false)
+            if (user) resolve(user)
+        });
+    });
+}
