cbe187624c9e4226708cb5229f9c984438b95305
git <gitit1@msn.com>
2021-04-16 19:53:26 +0300

    changed api configuration to set token only on production

--- server/config/thetvdb_api_configuration.js
+++ server/config/thetvdb_api_configuration.js
@@ -1,4 +1,3 @@
-// TODO: fix it so i can save the token
 const axios = require('axios');
 const fs = require('fs');
 
@@ -6,47 +5,46 @@ exports.setToken = async () => {
    console.log('Controller: setToken')
 
    let token = process.env.API_TOKEN;
+   let env = process.env.ENV;
    const apiUrl = process.env.API_URL;
 
-   // if (!token) {
+   if (env !== 'Development') {
+      try {
+         const res = await axios({
+            method: 'post',
+            url: `${apiUrl}/login`,
+            data: JSON.stringify({
+               "apikey": process.env.API_KEY,
+               "pin": process.env.API_PIN
+            }),
+            headers: {
+               'Content-Type': 'application/json',
+               'Origin': 'thetvdb.com'
+            }
+         });
 
-   try {
-      const res = await axios({
-         method: 'post',
-         url: `${apiUrl}/login`,
-         data: JSON.stringify({
-            "apikey": process.env.API_KEY,
-            "pin": process.env.API_PIN
-         }),
-         headers: {
-            'Content-Type': 'application/json',
-            'Origin': 'thetvdb.com'
-         }
-      });
+         token = res.data.data.token;
 
-      token = res.data.data.token;
+         fs.readFile('variables.env', 'utf8', function (err, data) {
+            if (err) {
+               console.log(err)
+            };
+            let linesExceptLast = data.split('\n')
 
-      fs.readFile('variables.env', 'utf8', function (err, data) {
-         if (err) {
-            console.log(err)
-         };
-         let linesExceptLast = data.split('\n')
-
-         linesExceptLast = linesExceptLast.slice(0, linesExceptLast.length - 1).join('\n');
-         linesExceptLast = linesExceptLast + `\nAPI_TOKEN='${token}'`;
+            linesExceptLast = linesExceptLast.slice(0, linesExceptLast.length - 1).join('\n');
+            linesExceptLast = linesExceptLast + `\nAPI_TOKEN='${token}'`;
 
 
-         fs.writeFileSync('variables.env', linesExceptLast);
-      });
+            fs.writeFileSync('variables.env', linesExceptLast);
+         });
 
-      console.log('new token is set');
+         console.log('new token is set');
 
-   } catch (error) {
-      console.log(`Error: \n${error}`)
+      } catch (error) {
+         console.log(`Error: \n${error}`)
+      }
+   } else {
+      console.log('token is already established')
    }
-
-   // }else{
-   //    console.log('token is already established')
-   // }
 
 };
